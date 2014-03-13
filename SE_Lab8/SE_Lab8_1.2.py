1701beda1dcbda105de022359605cfc19fca2212
Samacharn Sankul <smaamya@windowslive.com>
2014-03-13 20:48:26 +0700

    Lab 3

--- SE_Lab8/SE_Lab8_1.2.py
+++ SE_Lab8/SE_Lab8_1.2.py
@@ -17,11 +17,44 @@ class Simple_drawing_window1(QWidget):
 
         p.end()
 
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing 2")
+    
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(255, 255, 0))
+        p.setBrush(QColor(255, 255, 0))
+        p.drawPie(50, 50, 200, 200, 0, 360 * 16)
+
+        p.end()
+
+class Simple_drawing_window3(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing 3")
+    
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 0, 128))
+        p.drawPolygon([QPoint(200, 50), QPoint(200, 250), QPoint(600, 250), QPoint(450,50)])
+        p.end()
+
 def main():
     app = QApplication(sys.argv)
 
-    w = Simple_drawing_window1()
-    w.show()
+    w1 = Simple_drawing_window1()
+    w1.show()
+    w2 = Simple_drawing_window2()
+    w2.show()
+    w3 = Simple_drawing_window3()
+    w3.show()
 
     return app.exec_()
 
